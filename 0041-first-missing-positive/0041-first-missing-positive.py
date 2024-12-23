5d68880e1dd6cdbc6ec308e4028ad84952b3442b
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2024-12-23 19:32:23 +0530

    Time: 27 ms (80.8%), Space: 34.8 MB (5.82%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        s=set(nums)
+        for i in range(1,len(nums)+2):
+            if i not in s:
+                return i
