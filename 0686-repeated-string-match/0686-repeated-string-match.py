429388c20cf05ebd97a5644ddbd61c703ea3e714
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2025-01-08 22:45:31 +0530

    Time: 0 ms (100%), Space: 17.9 MB (23.74%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        n=ceil(len(b)/len(a))
+        if b in a*n:
+            return n
+        elif b in a*(n+1):
+            return n+1
+        return -1
