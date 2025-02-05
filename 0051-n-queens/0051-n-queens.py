c1dbf7293797e4473f3feb57ad1da2c71d0c4416
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2025-02-05 23:39:20 +0530

    Time: 35 ms (25.79%), Space: 18.2 MB (70.19%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,41 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def check(mat,n,row,col):
+            # Check vertical up
+            for i in range(row, -1, -1):
+                if mat[i][col] == 'Q':
+                    return False
+
+            # Check left diagonal
+            i, j = row, col
+            while i >= 0 and j >= 0:
+                if mat[i][j] == 'Q':
+                    return False
+                i -= 1
+                j -= 1
+
+            # Check right diagonal
+            i, j = row, col
+            while i >= 0 and j < n:
+                if mat[i][j] == 'Q':
+                    return False
+                i -= 1
+                j += 1
+
+            return True
+        
+
+        def solve(mat,n,row):
+            if row==n:
+                result.append(["".join(row) for row in mat])
+
+            else:
+                for i in range(n):
+                    if check(mat,n,row,i):
+                        mat[row][i]="Q"
+                        solve(mat,n,row+1)
+                        mat[row][i]='.'
+        result=[]
+        board=[['.' for _ in range(n)] for _ in range(n)]
+        solve(board,n,0)
+        return result
