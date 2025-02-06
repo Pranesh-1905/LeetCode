efcaaaad319cb7b1fe9240610d95359f76ca6010
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2025-02-06 21:16:55 +0530

    Time: 39 ms (14.68%), Space: 18.3 MB (5.59%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def check(mat,n,row,col):
+            i,j=row,col
+            while j>=0 and i>=0:
+                if mat[i][j]=="Q":
+                    return False
+                i-=1
+            
+            i,j=row,col
+            while j>=0 and i>=0:
+                if mat[i][j]=="Q":
+                    return False
+                i-=1
+                j-=1
+            
+            i,j=row,col
+            while j<n and i>=0:
+                if mat[i][j]=="Q":
+                    return False
+                i-=1
+                j+=1
+            return True   
+            
+
+        def solve(mat,n,row):
+            if row==n:
+                res.append(["".join(row) for row in mat])
+            else:
+                for col in range(n):
+                    if check(mat,n,row,col):
+                        mat[row][col]='Q'
+                        solve(mat,n,row+1)
+                        mat[row][col]='.'
+        res=[]
+        board=[["." for i in range(n)] for i in range(n)]
+        solve(board,n,0)
+        return len(res)
