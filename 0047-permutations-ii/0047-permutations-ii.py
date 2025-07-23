6a2312fa2de39a340b4d71b21a95313b1dfbc8ae
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2025-07-23 22:58:26 +0530

    Time: 5 ms (65.73%), Space: 18 MB (78.82%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,6 +1,20 @@
-from itertools import permutations
 class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        a=permutations(nums)
-        return list(set(a))
+    def permuteUnique(self, nums):
+        def backtrack(start):
+            if start == len(nums):
+                ans.append(nums[:])
+                return
+            
+            used = set()
+            for i in range(start, len(nums)):
+                if nums[i] in used:
+                    continue 
+                used.add(nums[i])
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start] 
         
+        nums.sort()  
+        ans = []
+        backtrack(0)
+        return ans
