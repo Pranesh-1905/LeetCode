a4fad26f4c4d726708167af13e4e6f43705c3da0
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2025-10-04 14:21:28 +0530

    Time: 3 ms (87.31%), Space: 18.8 MB (84.26%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp=[0]*(len(triangle)+1)
+        dp=[0]*(len(triangle)+1) #bottom up approach creating the last row +1 [0,0,0,0,0]
         for row in triangle[::-1]:
             for i,n in enumerate(row):
-                dp[i]=n+min(dp[i],dp[i+1])
+                dp[i]=n+min(dp[i],dp[i+1])# adding current number with the prev min of the diagonals
         return dp[0]
