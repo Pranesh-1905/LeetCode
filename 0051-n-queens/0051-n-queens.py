fb9544298bcadb2dfeb1bf7bb4d17075fce8ce4c
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2025-11-08 22:17:34 +0530

    Time: 39 ms (19.05%), Space: 18.1 MB (96.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,42 +1,38 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         def check(mat,n,row,col):
-            # Check vertical up
-            i, j = row, col
-            while i >= 0 and j >= 0:
-                if mat[i][j] == 'Q':
+            i,j=row,col
+            while j>=0 and i>=0:
+                if mat[i][j]=="Q":
                     return False
-                i -= 1
-            # Check left diagonal
-            i, j = row, col
-            while i >= 0 and j >= 0:
-                if mat[i][j] == 'Q':
+                i-=1
+            
+            i,j=row,col
+            while j>=0 and i>=0:
+                if mat[i][j]=="Q":
                     return False
-                i -= 1
-                j -= 1
-
-            # Check right diagonal
-            i, j = row, col
-            while i >= 0 and j < n:
-                if mat[i][j] == 'Q':
+                i-=1
+                j-=1
+            
+            i,j=row,col
+            while j<n and i>=0:
+                if mat[i][j]=="Q":
                     return False
-                i -= 1
-                j += 1
-
-            return True
-        
+                i-=1
+                j+=1
+            return True   
+            
 
         def solve(mat,n,row):
             if row==n:
-                result.append(["".join(row) for row in mat])
-
+                res.append(["".join(row) for row in mat])
             else:
-                for i in range(n):
-                    if check(mat,n,row,i):
-                        mat[row][i]="Q"
+                for col in range(n):
+                    if check(mat,n,row,col):
+                        mat[row][col]='Q'
                         solve(mat,n,row+1)
-                        mat[row][i]='.'
-        result=[]
-        board=[['.' for _ in range(n)] for _ in range(n)]
+                        mat[row][col]='.'
+        res=[]
+        board=[["." for i in range(n)] for i in range(n)]
         solve(board,n,0)
-        return result
+        return res
