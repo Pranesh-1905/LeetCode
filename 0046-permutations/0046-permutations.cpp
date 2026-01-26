b15351309223b26e382f9256eb680539e4e4d60d
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2026-01-26 12:15:59 +0530

    Time: 2 ms (37.48%), Space: 10.5 MB (69.91%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    void perm(int l,int r,vector<int> &nums,vector<vector<int>> &ans){
-        if(l==r){
-            ans.push_back(nums);
-        }
-        else{
-            for(int i=l;i<=r;i++){
-                swap(nums[l],nums[i]);
-                perm(l+1,r,nums,ans);
-                swap(nums[l],nums[i]);
-            }
-        }
-    }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        perm(0,nums.size()-1,nums,ans);
-        return ans;
+    sort(nums.begin(),nums.end());
+    vector<vector<int>> res;
+    do{
+        res.push_back(nums);
+    }while(next_permutation(nums.begin(),nums.end()));
+    return res;
     }
 };
