ed12589b61c8e7dd9b690ec2db9ef186232b3d54
Pranesh J S <152673369+Pranesh-1905@users.noreply.github.com>
2026-02-03 19:41:50 +0530

    Time: 0 ms (100%), Space: 10.7 MB (53.5%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int start, int end) {
+        int len = end - start + 1;
+        vector<int> dp(len);
+
+        dp[0] = nums[start];
+        dp[1] = max(nums[start], nums[start + 1]);
+
+        for (int i = 2; i < len; i++) {
+            dp[i] = max(nums[start + i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[len - 1];
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+
+        int case1 = solve(nums, 0, n - 2);//exclude 1sst house
+
+        int case2 = solve(nums, 1, n - 1);//exclude last house
+
+        return max(case1, case2);
+    }
+};
